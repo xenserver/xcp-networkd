302061a8098323003ee6956425d6439bdf75e1dc
Christian Lindig <christian.lindig@citrix.com>
2016-05-13 15:05:51 +0100

    CP-17268 put coverage setup code in (small) common lib in profiling/
    The code to set up profiling is used from several binaries. It does not
    harm to compile it into a non-profiled binary.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- networkd/networkd.ml
+++ networkd/networkd.ml
@@ -53,32 +53,7 @@ let options = [
 	"monitor_blacklist", Arg.String (fun x -> Network_monitor_thread.monitor_blacklist := String.split ',' x), (fun () -> String.concat "," !Network_monitor_thread.monitor_blacklist), "List of prefixes of interface names that are not to be monitored";
 ]
 
-(** This just sets up an environment variable and has no effect unless
- *  we are compiling for profiling *)
 
-let setup_coverage_profiling name =
-  let (//) = Filename.concat in
-  let tmpdir =
-    let getenv n   = try Sys.getenv n with Not_found -> "" in
-    let dirs    = 
-      [ getenv "TMP"
-      ; getenv "TEMP"
-      ; "/tmp"
-      ; "/usr/tmp"
-      ; "/var/tmp"
-      ] in
-    let is_dir  = function 
-    | ""    -> false
-    | path  -> try Sys.is_directory path with Sys_error _ -> false
-    in try
-      List.find is_dir dirs
-    with
-      Not_found -> D.error "can't find temp directory %s" __LOC__; exit 1
-  in try 
-    ignore (Sys.getenv "BISECT_FILE") 
-  with Not_found ->
-    Unix.putenv "BISECT_FILE" (tmpdir // Printf.sprintf "bisect-%s-" name)
- 
 let start server =
 	Network_monitor_thread.start ();
 	Network_server.on_startup ();
@@ -104,7 +79,7 @@ let doc = String.concat "\n" [
 ]
 
 let _ =
-  setup_coverage_profiling "networkd";
+  Coverage.init "networkd";
 	begin match Xcp_service.configure2
 		~name:Sys.argv.(0)
 		~version:Version.version
